2aee192145a865b1533aad7b6710b5af7afa7192
MBALI2502 <damobaly@gmail.com>
2025-03-03 12:05:14 +0100

    Mise à jour de script2.py sur branche2

--- script2.py
+++ script2.py
@@ -1,1 +1,10 @@
-print("Ceci est le script 2 sur branche2")
+# Demander deux nombres à l'utilisateur
+num1 = float(input("Entrez le premier nombre : "))
+num2 = float(input("Entrez le deuxième nombre : "))
+
+# Calcul de la somme
+somme = num1 + num2
+
+# Affichage du résultat
+print(f"La somme de {num1} et {num2} est {somme}")
+

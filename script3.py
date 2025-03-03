620d765b82452201fbb631452a84d94da86d47a0
MBALI2502 <damobaly@gmail.com>
2025-03-03 12:07:38 +0100

    Mise à jour de script3.py sur branche3

--- script3.py
+++ script3.py
@@ -1,1 +1,9 @@
-print("Ceci est le script 3 sur branche3")
+# Demander un nombre à l'utilisateur
+nombre = int(input("Entrez un nombre entier : "))
+
+# Vérifier si le nombre est pair ou impair
+if nombre % 2 == 0:
+    print(f"Le nombre {nombre} est pair.")
+else:
+    print(f"Le nombre {nombre} est impair.")
+
